12e8a9a9c2400e26f93a09557317c142e0da8e90
Bruno <brunodaniel.elias@gmail.com>
2023-12-14 16:04:04 -0300

    feat: create initial config

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import { env } from "./env"
+import { knex as setupKnex, Knex } from "knex"
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set")
+}
+
+export const knexConfig: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite3"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+}
+
+export const knex = setupKnex(knexConfig)
